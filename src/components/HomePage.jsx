dd5893aff9574c268dcfe9a08e940b186ab93d63
Steffani Silva <silvasteffani2004@gmail.com>
2026-01-05 01:39:00 +0530

    Deploy
    Deploy

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Home, TrendingUp, Users } from 'lucide-react';
+import { Home, TrendingUp, Users } from "lucide-react";
 import './HomePage.css';
 import SocialAside from './SocialAside';
 
